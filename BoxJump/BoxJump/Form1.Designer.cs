1060c9d64413d1bef285b64fc3222d1856f6c41f
Dilan Patel <DPatel@spartaglobal.com>
2019-11-10 12:54:13 +0000

    Created player and floor asset, added player movement, floor detection

---
+++ BoxJump/BoxJump/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace BoxJump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.YellowGreen;
+            this.floor.Location = new System.Drawing.Point(-8, 563);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(498, 50);
+            this.floor.TabIndex = 0;
+            this.floor.TabStop = false;
+            this.floor.Tag = "floor";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Aquamarine;
+            this.player.Location = new System.Drawing.Point(227, 513);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 50);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 603);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.floor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BoxJump/BoxJump/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxJump
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft = false;
+        bool goRight = false;
+        bool goUp = false;
+
+        int jumpSpeed = 10;
+        int force = 8;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up && !goUp)
+            {
+                goUp = true;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (goUp)
+            {
+                goUp = false;
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            player.Top += jumpSpeed;
+            if (goUp && force < 0)
+            {
+                goUp = false;
+            }
+
+            if (goLeft)
+            {
+                player.Left -= 5;
+            }
+
+            if (goRight)
+            {
+                player.Left += 5;
+            }
+
+            if (goUp)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "floor")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds) && !goUp)
+                    {
+                        force = 8;
+                        player.Top = x.Top - player.Height;
+                    }
+                }
+            }
+        }
+    }
+}
